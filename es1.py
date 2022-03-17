9bf9755abd8b262edfd80437d4ba8f2e5e1f11c1
Musta16 <mostafaaboukelila@gmail.com>
2022-03-17 12:07:33 +0100

    Create ad input ad appen on list

---
+++ es1.py
@@ -0,0 +1,8 @@
+listaNum = [] #creo una lista vuota dove inserire i numeri
+
+
+inputUtente = input("Inserisci un numero: ") #prendo l'input dell'utente
+listaNum.append(inputUtente) #inserisco l'input nella lista
+
+print(listaNum)
+
