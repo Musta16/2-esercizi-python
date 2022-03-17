3b452a14046063fd9367058535a0a64c0a91b8ae
Musta16 <mostafaaboukelila@gmail.com>
2022-03-17 12:50:03 +0100

    - created LIST with a length chosen by user - created FOR to store numeric values into LIST(converted in float) - created FOR to sum all values in LIST - imported math - insert ad avg formula

---
+++ es2.py
@@ -0,0 +1,18 @@
+import math
+
+lenLista = int(input("Quanti numeri vuoi inserire dentro alla lista? ")) #chiedo la lungezza della lista
+l = [] #creo una lista
+for i in range(lenLista): #ciclo for per inserire dentro la lista n valori decisi da utente
+    num_String = input("Insersci un numero: ")
+    num = float(num_String) #converto la stringa di numeri in float
+    l.append(num)
+
+somma = 0 #imposto la somma a zero
+for i in range(len(l)): #ciclo per sommare tutti i numeri dentro la lista
+    somma += l[i] #sommo "somma" al numero corrispondende all'indice
+
+media = somma/lenLista
+
+print("La media è: ", media)
+print("Lista: ", l)
+print("Lungezza lista: ", len(l)) #printo lunghezza lista
