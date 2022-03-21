7383005ca8d3e62dd00a6bdf0f27f26dde67d2a6
Musta16 <mostafaaboukelila@gmail.com>
2022-03-21 14:21:39 +0100

    - inserted a FOR with an IF to check if a number is already in the list

--- es8.py
+++ es8.py
@@ -4,8 +4,14 @@
 # 1. usando solo le liste
 
 lenLista = int(input("Quanti numeri vuoi inserire dentro alla lista? ")) #chiedo la lungezza della lista
-l = [] #creo una lista
+listaConDuplicati = [] #creo una lista
 for i in range(lenLista): #ciclo for per inserire dentro la lista n valori decisi da utente
     num_String = input("Insersci un numero: ")
     num = float(num_String) #converto la stringa di numeri in float
-    l.append(num)
+    listaConDuplicati.append(num)
+
+listaUnica = [] #creo una lista vuota
+for i in listaConDuplicati:
+    if i not in listaUnica: # appendi se il numero NON è già presente all'interno della nuova lista
+        listaUnica.append(i)
+print(listaUnica)
