f65fb3bc3f4d41d5689727b2ba4270429ed654af
Musta16 <mostafaaboukelila@gmail.com>
2022-03-31 12:32:01 +0200

    - inserted the problem - fixed the input by adding an input check

--- es2.py
+++ es2.py
@@ -1,20 +1,38 @@
+# Memorizzare una serie di numeri presi in input dall’utente.
+# Mostrarne media e deviazione standard (import math, math.sqrt() per radice quadrata)
 import math
 
-lenLista = int(input("Quanti numeri vuoi inserire dentro alla lista? ")) #chiedo la lungezza della lista
-l = [] #creo una lista
-for i in range(lenLista): #ciclo for per inserire dentro la lista n valori decisi da utente
-    num_String = input("Insersci un numero: ")
-    num = float(num_String) #converto la stringa di numeri in float
-    l.append(num)
+while True:  # ciclo di controllo input
+    try:
+        lenLista = int(input("Quanti numeri vuoi inserire dentro alla lista? "))  # chiedo la lungezza della lista
+    except ValueError:  # eccezione
+        print("Non hai inserito un numero! Ritenta!")
+        continue
+    else:
+        break
 
-somma = 0 #imposto la somma a zero
-for i in range(len(l)): #ciclo per sommare tutti i numeri dentro la lista
-    somma += l[i] #sommo "somma" al numero corrispondende all'indice
+l = []  # creo una lista
 
-media = somma/lenLista
+for i in range(lenLista):  # ciclo for per inserire dentro la lista n valori decisi da utente
+    while True:  # ciclo di controllo input
+        try:
+            num = float(input("Insersci un numero: "))
+        except ValueError:  # eccezione
+            print("Non hai inserito un numero! Ritenta!")
+            continue
+        else:
+            break
+
+    l.append(num)  # inserisco il numero dentro la lista
+
+somma = 0  # imposto la somma a zero
+for i in range(len(l)):  # ciclo per sommare tutti i numeri dentro la lista
+    somma += l[i]  # sommo "somma" al numero corrispondende all'indice
+
+media = somma / lenLista
 devStandFalsa = math.sqrt(somma / lenLista)
 
 print("La media è: ", media)
 print("La deviazine standard(falsa) è: ", devStandFalsa)
 print("Lista: ", l)
-print("Lungezza lista: ", len(l)) #printo lunghezza lista
+print("Lungezza lista: ", len(l))  # printo lunghezza lista
