6e24cead1f12baae5c7fbf18bfddaf1bf9175fcb
Musta16 <mostafaaboukelila@gmail.com>
2022-04-04 11:58:49 +0200

    update

--- es8.py
+++ es8.py
@@ -1,21 +1,38 @@
 # Memorizzare una serie di numeri con duplicati presi in input dall'utente
 # Mostrare solo i numeri univoci della serie: 1. usando solo le liste - 2. usando solo i set
 
+# chiedo lunghezza lista
+while True:  # ciclo di controllo input
+    try:
+        lenLista = int(input("Quanti numeri vuoi inserire dentro le liste? "))  # chiedo la lungezza della lista
+    except ValueError:  # eccezione
+        print("Non hai inserito un numero! Ritenta!")
+        continue
+    else:
+        break
+
+# compilo lista
+l1 = []  # creo una lista
+for i in range(lenLista):  # ciclo for per inserire dentro la lista n valori decisi da utente
+    while True:  # ciclo di controllo input
+        try:
+            num = float(input("Insersci un numero: "))
+        except ValueError:  # eccezione
+            print("Non hai inserito un numero! Ritenta!")
+            continue
+        else:
+            break
+    l1.append(num)  # inserisco il numero dentro la lista
+
 # 1. usando solo le liste
 
-lenLista = int(input("Quanti numeri vuoi inserire dentro alla lista? ")) #chiedo la lungezza della lista
-listaConDuplicati = [] #creo una lista
-for i in range(lenLista): #ciclo for per inserire dentro la lista n valori decisi da utente
-    num_String = input("Insersci un numero: ")
-    num = float(num_String) #converto la stringa di numeri in float
-    listaConDuplicati.append(num)
-
-listaUnica = [] #creo una lista vuota
-for i in listaConDuplicati:
-    if i not in listaUnica: # appendi se il numero NON è già presente all'interno della nuova lista
+listaUnica = []
+for i in l1:
+    if i not in listaUnica:
         listaUnica.append(i)
-print(listaUnica)
+
+print("Lista numeri univoci:", listaUnica)
 
 # 2. usando solo i set
 
-print(set(listaConDuplicati)) #facendo il set della listaConDubplicati elimina le ripetizioni
+print("Set numeri univoci:", set(l1))
